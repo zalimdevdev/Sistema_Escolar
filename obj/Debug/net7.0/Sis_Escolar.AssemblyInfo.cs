232101d1af7b1b00bf19e06fe89a86cf970347c0
zalimdevdev <zalimdevdev@hotmail.com>
2024-10-03 20:33:44 -0600

    segunda subida

--- obj/Debug/net7.0/Sis_Escolar.AssemblyInfo.cs
+++ obj/Debug/net7.0/Sis_Escolar.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sis_Escolar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+910156777dca4f646ff95696794e2637e6489407")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sis_Escolar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sis_Escolar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
